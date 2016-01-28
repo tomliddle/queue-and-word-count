c8647fb51bbce77b46c2447e8ac0a25c860f1d7e
Tom Liddle <tom.liddle@gmail.com>
2016-01-28 15:50:15 +0000

    Added tests for count test - toWords fn only

--- src/test/scala/counttest.scala
+++ src/test/scala/counttest.scala
@@ -14,6 +14,54 @@ class counttest extends WordSpec with Matchers with BeforeAndAfterEach{
 				CountCharacters.toWords(9) should equal("nine")
 			}
 
+			"0 should equal zero" in {
+				CountCharacters.toWords(0) should equal("zero")
+			}
+
+			"10 should equal ten" in {
+				CountCharacters.toWords(10) should equal("ten")
+			}
+
+			"11 should equal eleven" in {
+				CountCharacters.toWords(11) should equal("eleven")
+			}
+
+			"155 should equal one hundred fifty five" in {
+				CountCharacters.toWords(155) should equal("one hundred fifty five")
+			}
+
+			"7777 should equal seven thousand seven hundred seventy seven" in {
+				CountCharacters.toWords(7777) should equal("seven thousand seven hundred seventy seven")
+			}
+
+			"67673 should equal sixty seven thousand six hundred seventy three" in {
+				CountCharacters.toWords(67673) should equal("sixty seven thousand six hundred seventy three")
+			}
+
+			"100000 should equal one hundred thousand" in {
+				CountCharacters.toWords(100000) should equal("one hundred thousand")
+			}
+
+			"100050 should equal one hundred thousand and fifty" in {
+				CountCharacters.toWords(100050) should equal("one hundred thousand and fifty")
+			}
+
+			"1700000 should equal one million seven hundred thousand" in {
+				CountCharacters.toWords(1700000) should equal("one million seven hundred thousand")
+			}
+
+			"10000000 should equal one hundred million" in {
+				CountCharacters.toWords(10000000) should equal("one hundred million")
+			}
+
+			"100000000 should equal one billion" in {
+				CountCharacters.toWords(100000000) should equal("one billion")
+			}
+
+			"100100000 should equal one billion one million" in {
+				CountCharacters.toWords(100100000) should equal("one billion")
+			}
+
 
 
 		}
